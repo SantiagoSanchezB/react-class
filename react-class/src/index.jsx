cf84d8fdc9087ba21091639eed6d42218fd600da
SantiagoS <dssanchez752@misena.edu.co>
2021-08-27 09:27:26 -0500

    Agregate Functions index

--- react-class/src/index.jsx
+++ react-class/src/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/headerComponent';
-import Estudiante from './components/Estudiante';
 import Footer from './components/footerComponent';
 import Contenedor from './components/Contenedor';
 
