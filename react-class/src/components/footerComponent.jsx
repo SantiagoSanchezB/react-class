56b22dbd0c15e5fd066f630bb217e5579f74604d
SantiagoS <dssanchez752@misena.edu.co>
2021-09-03 10:09:39 -0500

    Modified footer and agregate other footer

--- react-class/src/components/footerComponent.jsx
+++ react-class/src/components/footerComponent.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import logoGit from'../github.svg';
+import '../css/index_css.css';
 
 const Footer = () => (
     <div>
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
-            <div class="col-md-4 d-flex align-items-center">
-                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"/>
-                <span class="text-muted">&copy; 2021 Santiago Sanchez</span>
+        <footer class="bg-light text-center text-lg-start">
+            <div class="text-center p-3">
+                © 2020 Copyright: Santiago Sanchez
+                <a class="text-muted bar" href="https://github.com/SantiagoSanchezB"><img src={logoGit}/></a>
             </div>
-            <ul class="nav col-md-4 justify-content-end list-unstyled">
-                <li class="ms-3"><a class="text-muted" href="https://github.com/SantiagoSanchezB"><img src={logoGit}/></a></li>
-                <li class="ms-3"><a class="text-muted" href="#"></a></li>
-                <li class="ms-3"><a class="text-muted" href="#"></a></li>
-            </ul>
         </footer>
     </div>
 );
