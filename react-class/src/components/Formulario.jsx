dcfa39a2f5bcc34c88aa69b665c1065d7c9b61f2
SantiagoS <dssanchez752@misena.edu.co>
2021-09-03 10:10:57 -0500

    Creation form component

---
+++ react-class/src/components/Formulario.jsx
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+
+class Formulario extends Component{
+    constructor (props){
+        super(props)
+
+        this.state = {
+            nombre : "",
+            apellido: ""
+        }
+        this.cambiarNombre = this.cambiarNombre.bind(this);
+        this.cambiarApellido = this.cambiarApellido.bind(this);
+    }
+
+    cambiarNombre(e){
+        this.setState({
+            Nombre: e.target.value
+        });
+    }
+
+    cambiarApellido(e){
+        this.setState({
+            Apellido: e.target.value
+        });
+    }
+
+    render(){
+        return(
+            <div id="Elemento">
+                <h1>Formulario</h1>
+                <form action="">
+                    <label htmlFor="">Nombre</label>
+                    <input class="form-control name" type="text" name="nombre" placeholder="Ingrese el nombre" onChange={
+                        (i) => this.setState({
+                            nombre: i.target.value
+                        })
+                    }/>
+                    <div id="emailHelp" class="form-text">{`Nombre: ${this.state.nombre}`}</div><br/>
+
+                    <label htmlFor="">Apellido</label>
+                    <input class="form-control name" type="text" name="apellido" placeholder="Ingrese el nombre" onChange={
+                        (i) => this.setState({
+                            apellido: i.target.value
+                        })
+                    }/>
+                    <div id="emailHelp" class="form-text">{`Apellido: ${this.state.apellido}`}</div><br/>
+                    <hr class="dropdown-divider name"/>
+                    <input class="btn btn-success" type="submit" value="Enviar"/>
+                </form>
+            </div>
+        )
+    }
+
+    componentDidMount(){
+        let Elemento = document.getElementById("Elemento")
+        console.log(Elemento)
+    }
+
+    componentDidUpdate(prevProps, prevState){ 
+        console.log(prevState);
+        console.log(prevProps);
+        console.log("-----------------------")
+    }
+}
+
+export default Formulario;
