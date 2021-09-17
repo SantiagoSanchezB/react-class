8561131cbbc5f5305fb5a470d2f150ed56505bfa
SantiagoS <dssanchez752@misena.edu.co>
2021-09-17 08:29:21 -0500

    New Navbar for all pages

--- react-class/src/components/headerComponent.jsx
+++ react-class/src/components/headerComponent.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 
 const Header = () => (
-    <div className = "">
-        <nav class="navbar navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand">React Class</a>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
-                    <button class="btn btn-outline-success" type="submit">Buscar</button>
-                </form>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">React-Class</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Estudiantes">Estudiantes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Contacto">Contacto</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button class="btn btn-outline-light" type="submit">Search</button>
+            </form>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
 );
 
 export default Header;
