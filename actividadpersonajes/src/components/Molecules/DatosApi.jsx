6af2aa7239c3aa8bda5730fd2264402d0b170222
SantiagoS <dssanchez752@misena.edu.co>
2021-09-17 11:09:40 -0500

    New folder for API page

---
+++ actividadpersonajes/src/components/Molecules/DatosApi.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Component } from 'react';
+import '../../css/index_css.css';
+import Footer_A from '../Footer_A';
+import axios from 'axios';
+
+class DatosApi  extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            posts:[]
+        }
+    }
+
+    componentDidMount(){
+        axios.get('https://my-json-server.typicode.com/SantiagoSanchezB/DataJs-Plundere/Personajes')
+        .then(resp =>{
+            this.setState({
+                posts: resp.data
+            })
+        })
+    }
+
+    render(){
+        const {posts} = this .state;
+        return(
+            <>
+                <div>
+                    {
+                        posts.map( u => (
+                            <>
+                                <div class="Element">
+                                    <div className="card">
+                                        <div class="card-body">
+                                            <div class="aling pos img-fluid">
+                                                <img src={u.img} class="rounded-circle"/>
+                                            </div>
+                                            <h5 class="card-title"><b>Nombre:</b> {u.nombre}</h5>
+                                            <p><b>Edad:</b> {u.edad}</p>
+                                            <p><b>Count:</b> {u.count}</p>
+                                            <p><b>Especie:</b> {u.especie}</p>
+                                            <p><b>Arma:</b> {u.Arma}</p>
+                                        </div>
+                                    </div>
+                                </div> 
+                            </>
+                        ))
+                    }
+                </div>
+                <Footer_A class="footer"/>
+            </>
+        )
+    }
+}
+
+export default DatosApi;
